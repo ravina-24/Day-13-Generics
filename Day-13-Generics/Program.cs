f5e436d970c90d42a9b44dc7e2e68a71b7575187
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-22 22:59:19 +0530

    [Ravina] Add : Checking the maximum number between three integers using compareTo method

--- Day-13-Generics/Program.cs
+++ Day-13-Generics/Program.cs
@@ -6,7 +6,9 @@ namespace Day_13_Generics
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Find max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
         }
     }
 }
