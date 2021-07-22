8fd5a41eeaa351de8c8ef8a498ac0bcacf20bbe1
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-22 23:08:39 +0530

    [Ravina] Add : Checking the maximum number between three float values using compareTo method

--- Day-13-Generics/MaximumNumberCheck.cs
+++ Day-13-Generics/MaximumNumberCheck.cs
@@ -22,5 +22,22 @@ namespace Day_13_Generics
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
--- Day-13-Generics/Program.cs
+++ Day-13-Generics/Program.cs
@@ -9,6 +9,8 @@ namespace Day_13_Generics
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(output);
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
         }
     }
 }
