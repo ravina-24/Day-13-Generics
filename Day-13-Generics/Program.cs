524e5b2f06d53a9ba52ea95c64ed57fd333eb5b5
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-23 12:29:34 +0530

    [Ravina] Add : Checking the maximum between three String  using compareTo method

--- Day-13-Generics/Program.cs
+++ Day-13-Generics/Program.cs
@@ -11,7 +11,7 @@ namespace Day_13_Generics
             Console.WriteLine(output);
             double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
             Console.WriteLine(doubleoutput);
-            string strigoutput = MaximumNumberCheck.MaximumStringNumber("22", "33", "44");
+            string strigoutput = MaximumNumberCheck.MaximumStringNumber("apple", "banana", "peach");
             Console.WriteLine(strigoutput);
         }
     }
